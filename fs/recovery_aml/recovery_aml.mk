36232f899c20e29c82269fbf6eaf4f4a2e0f8778
Stanislav Vlasic <svlasic@gmail.com>
2013-08-21 01:18:34 +0200

    [recovery_aml] Add custom uboot splashscreen and some other cosmetics

--- fs/recovery_aml/recovery_aml.mk
+++ fs/recovery_aml/recovery_aml.mk
@@ -14,6 +14,22 @@ ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_WIPE_USERDATA_CONDITIONAL),y)
   RECOVERY_AML_ARGS += -c
 endif
 
+# We will initialize some vars here
+AML_LOGO := fs/recovery_aml/aml_logo.img
+UPDATE_PACKAGE := update.img
+UPDATE_UNSIGNED := update-unsigned.img
+
+# Set AML_LOGO if specified
+ifneq ($(strip $(BR2_TARGET_AML_LOGO)),"")
+  AML_LOGO := $(BR2_TARGET_AML_LOGO)
+endif
+
+# If boardname is f16ref, set update file with zip extension
+ifeq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_AML_BOARDNAME)),"f16ref")
+  UPDATE_PACKAGE := update.zip
+  UPDATE_UNSIGNED := update-unsigned.zip
+endif
+
 define ROOTFS_RECOVERY_AML_CMD
  mkdir -p $(BINARIES_DIR)/aml_recovery/system && \
  tar -C $(BINARIES_DIR)/aml_recovery/system -xf $(BINARIES_DIR)/rootfs.tar && \
@@ -21,15 +37,15 @@ define ROOTFS_RECOVERY_AML_CMD
  PYTHONDONTWRITEBYTECODE=1 $(HOST_DIR)/usr/bin/python fs/recovery_aml/android_scriptgen $(RECOVERY_AML_ARGS) -i -p $(BINARIES_DIR)/aml_recovery/system -o \
    $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/updater-script && \
  cp -f fs/recovery_aml/update-binary $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ && \
- cp -f fs/recovery_aml/aml_logo.img $(BINARIES_DIR)/aml_recovery/ && \
+ cp -f $(AML_LOGO) $(BINARIES_DIR)/aml_recovery/aml_logo.img && \
  cp -f $(BINARIES_DIR)/uImage-* $(BINARIES_DIR)/aml_recovery/ && \
  find $(BINARIES_DIR)/aml_recovery/system/ -type l -delete && \
  find $(BINARIES_DIR)/aml_recovery/system/ -type d -empty -exec sh -c 'echo "dummy" > "{}"/.empty' \; && \
  pushd $(BINARIES_DIR)/aml_recovery/ >/dev/null && \
- zip -m -q -r -y $(BINARIES_DIR)/aml_recovery/update-unsigned.img aml_logo.img uImage-2.6.34 META-INF system && \
+ zip -m -q -r -y $(BINARIES_DIR)/aml_recovery/$(UPDATE_UNSIGNED) aml_logo.img uImage-2.6.34 META-INF system && \
  popd >/dev/null && \
- echo "Signing update.img..." && \
- pushd fs/recovery_aml/ >/dev/null; java -Xmx1024m -jar signapk.jar -w testkey.x509.pem testkey.pk8 $(BINARIES_DIR)/aml_recovery/update-unsigned.img $(BINARIES_DIR)/update.img && \
+ echo "Signing $(UPDATE_PACKAGE)..." && \
+ pushd fs/recovery_aml/ >/dev/null; java -Xmx1024m -jar signapk.jar -w testkey.x509.pem testkey.pk8 $(BINARIES_DIR)/aml_recovery/$(UPDATE_UNSIGNED) $(BINARIES_DIR)/$(UPDATE_PACKAGE) && \
  rm -rf $(BINARIES_DIR)/aml_recovery; rm -f $(TARGET_DIR)/usr.sqsh
 endef
 
